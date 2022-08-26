04512660ed2fb47a89cd629f041ea68ac64b3fd6
abragtim <timurkatime@gmail.com>
2022-08-26 23:07:18 +0200

    tests file, symple Makefile, some format changes

--- test_sport_support_tbot.py
+++ test_sport_support_tbot.py
@@ -1,9 +1,16 @@
 import unittest
-import sport_support_tbot
+import sport_support_tbot as tbot
 
 
+# TODO: tests
 class TestSportBot(unittest.TestCase):
-    def test_some(self):
+    def test_tbot_welcome_message(self):
+        pass
+
+    def test_tbot_set_sport_program(self):
+        pass
+
+    def test_tbot_start_sport_program(self):
         pass
 
 
