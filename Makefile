ca24fd4a3e27648cf9efc281d4c12bc8d107f6f7
abragtim <timurkatime@gmail.com>
2022-08-26 22:10:46 +0200

    testfile

---
+++ Makefile
@@ -0,0 +1,5 @@
+run:
+	python3 sport_support_tbot.py
+
+test:
+	python3 test_sport_support_tbot.py
---
+++ test_sport_support_tbot.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestSportBot(unittest.TestCase):
+    def test_some(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
